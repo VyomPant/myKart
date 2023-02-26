646744dfda0024f1c76bd813d17748225709a3bd
Vyom Pant <vyompant14@gmail.com>
2023-02-26 16:40:11 +0530

    Added sync communication between Order Service and Inventory Service

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.programmingtechie.orderservice.constants;
+
+public class Constants {
+    public static final String Get_Inventory_Endpoint ="http://localhost8082/api/inventory";
+}
--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.orderservice.service;
 
+import com.programmingtechie.orderservice.dto.InventoryResponse;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.model.Order;
@@ -8,9 +9,13 @@ import com.programmingtechie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static com.programmingtechie.orderservice.constants.Constants.Get_Inventory_Endpoint;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +23,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -30,7 +35,27 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service, and place order if product is in stock
+        // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
+        InventoryResponse[] inventoryResponsesList=webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsesList).allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is not in stock,order can't be placed");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
